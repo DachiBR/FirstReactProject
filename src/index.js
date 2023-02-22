44533f7c4f50cda43ba22f0d11ab4b896a016957
DachMan <d.bregadze028@gmail.com>
2023-02-23 00:45:40 +0400

    reactproject

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; /* its hooking our application to index.html */
+import App from '../src/app';
+import './index.css';
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
